ea229bd46585b34b0ae2a65371746cb9b5d5e63c
Jimmy Ortiz <33940849+Jimmond@users.noreply.github.com>
2022-06-29 13:51:49 +0000

    mod

--- myApp.js
+++ myApp.js
@@ -1,18 +1,24 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 
-
-console.log("Hello World");
+let jsonResponse = {"message":"Hello World"};
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+const mySecret = process.env['MESSAGE_STYLE']
 
 app.use("/public", express.static(__dirname + "/public"));
 
 
-
+app.get("/json", (req, res) => {
+  
+  if (mySecret === "uppercase") {
+  jsonResponse.message = jsonResponse.message.toUpperCase()
+} res.json(jsonResponse);
+});
 
 
 
